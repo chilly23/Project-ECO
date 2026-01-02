d88df5479176a757191e340db400509112f767d8
chilly23 <rainbowtailor84@gmail.com>
2026-01-03 04:23:20 +0530

    v2: update C:\Users\rainb\Downloads\content engine\frontend

--- frontend/scripts/config.js
+++ frontend/scripts/config.js
@@ -1,2 +1,4 @@
 export const SERP_API_KEY = "1ad782632695faabec325a992c1d5f2b890665ff0499bffe2ab88764bed301d6";
 export const OPENROUTER_KEY = "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";
+const API_KEY = "AIzaSyDObv6ZowtHDhU1zmhDSwalW_b-gZn5-j4";
+const CX = "3144ca98089234e28";
