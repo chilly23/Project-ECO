586a60fbdc0f78596be36349d60356cc0a3060ae
Velan E <100113908+chilly23@users.noreply.github.com>
2026-01-10 01:01:45 +0530

    Update config.js

--- frontend/api/config.js
+++ frontend/api/config.js
@@ -1,9 +1,9 @@
 export default function handler(req, res) {
   res.json({
-    GOOGLE_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_API_KEY,
-    GOOGLE_CX: process.env.NEXT_PUBLIC_GOOGLE_CX,
-    GOOGLE_API_KEY_2: process.env.NEXT_PUBLIC_GOOGLE_API_KEY_2,
-    GOOGLE_CX_2: process.env.NEXT_PUBLIC_GOOGLE_CX_2,
+    GOOGLE_API_KEY: process.env.NEXT_PUBLIC_API_KEY,
+    GOOGLE_CX: process.env.NEXT_PUBLIC_CX,
+    GOOGLE_API_KEY_2: process.env.NEXT_PUBLIC_API_KEY2,
+    GOOGLE_CX_2: process.env.NEXT_PUBLIC_CX, // if same CX
     OPENROUTER_KEY: process.env.NEXT_PUBLIC_OPENROUTER_KEY,
     APOLLO_KEY: process.env.NEXT_PUBLIC_APOLLO_KEY
   });
