3c84069606ab5544a90648c0312d9539343c8e18
Velan E <100113908+chilly23@users.noreply.github.com>
2026-01-09 14:18:31 +0530

    Update config.js

--- frontend/public/scripts/config.js
+++ frontend/public/scripts/config.js
@@ -1,5 +1,5 @@
 export const SERP_API_KEY = "1ad782632695faabec325a992c1d5f2b890665ff0499bffe2ab88764bed301d6";
-export const OPENROUTER_KEY = "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";
+export const OPENROUTER_KEY = "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";
 const API_KEY = "AIzaSyDObv6ZowtHDhU1zmhDSwalW_b-gZn5-j4";
 const CX = "3144ca98089234e28";
 
